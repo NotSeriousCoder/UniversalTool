9859bd976e6daaa6d1673e7b496fe0147bcadcc5
胡晓斌 <huxiaobin@bingosoft.net>
2018-09-21 15:52:44 +0800

    1.KeyBoardUtil增加软键盘状态检测

--- UniversalTool/utillib/src/main/java/com/bingor/utillib/KeyBoardUtil.java
+++ UniversalTool/utillib/src/main/java/com/bingor/utillib/KeyBoardUtil.java
@@ -2,6 +2,7 @@ package com.bingor.utillib;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Rect;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -41,5 +42,23 @@ public class KeyBoardUtil {
     public static void hideKeyBoard(Activity activity) {
         ((InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE))
                 .hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    /**
+     * 软键盘是否展开
+     *
+     * @param activity
+     * @return
+     */
+    public static boolean isSoftShowing(Activity activity) {
+        //获取当屏幕内容的高度
+        int screenHeight = activity.getWindow().getDecorView().getHeight();
+        //获取View可见区域的bottom
+        Rect rect = new Rect();
+        //DecorView即为activity的顶级view
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
+        //考虑到虚拟导航栏的情况（虚拟导航栏情况下：screenHeight = rect.bottom + 虚拟导航栏高度）
+        //选取screenHeight*2/3进行判断
+        return screenHeight * 2 / 3 > rect.bottom;
     }
 }
