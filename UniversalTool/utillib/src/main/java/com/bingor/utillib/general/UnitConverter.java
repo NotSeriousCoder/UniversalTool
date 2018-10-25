f29d42aa1afe0d9109c3f7c68f229b71698a2176
胡晓斌 <huxiaobin@bingosoft.net>
2018-10-25 15:54:06 +0800

    1.增加dp sp px转换工具

---
+++ UniversalTool/utillib/src/main/java/com/bingor/utillib/general/UnitConverter.java
@@ -0,0 +1,71 @@
+package com.bingor.utillib.general;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+/**
+ * dp/px/sp单位转换工具
+ */
+public class UnitConverter {
+    public static final int HORIZONTAL = 0;
+    public static final int VERTICAL = 1;
+
+    protected static DisplayMetrics displayMetrics;
+
+    public static DisplayMetrics getDisplayMetrics(Context context) {
+        if (displayMetrics == null) {
+            displayMetrics = context.getResources().getDisplayMetrics();
+        }
+        return displayMetrics;
+    }
+
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = getDisplayMetrics(context).density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = getDisplayMetrics(context).density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int dip2px(Context context, float dipValue, int orientation) {
+        if (orientation == HORIZONTAL) {
+            return (int) (dipValue * (getDisplayMetrics(context).widthPixels / 320.0));
+        } else {
+            return (int) (dipValue * (getDisplayMetrics(context).heightPixels / 480.0));
+        }
+    }
+
+    public static int px2dip(Context context, float pxValue, int orientation) {
+        if (orientation == HORIZONTAL) {
+            return (int) (pxValue / (getDisplayMetrics(context).widthPixels / 320.0));
+        } else {
+            return (int) (pxValue / (getDisplayMetrics(context).heightPixels / 480.0));
+        }
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = getDisplayMetrics(context).scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue
+     * @return
+     */
+
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = getDisplayMetrics(context).scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+}
